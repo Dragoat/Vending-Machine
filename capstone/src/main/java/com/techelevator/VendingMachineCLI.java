0bd1f94f431343f23d185a2227b6f6054576ea49
Charles Grantham <granthamj88@gmail.com>
2022-04-11 12:11:57 -0700

    attempt

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,34 +1,52 @@
 package com.techelevator;
 
+import com.techelevator.view.CompileItemInventory;
 import com.techelevator.view.Menu;
+import com.techelevator.view.Purchase;
+
+import java.util.Arrays;
 
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT};
 
+	public Boolean mainMenuSelected = true;
 	private Menu menu;
+	CompileItemInventory compile = new CompileItemInventory();
+	Purchase purchase = new Purchase(menu);
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 	}
 
 	public void run() {
-		while (true) {
+		while (mainMenuSelected) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
-
+			//maybe replace if's with a switch
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
+				// calls CompileItemInventory.java to display vending machine items
+				compile.printDisplay();
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+				purchase.purchaseMenuSelected = true;
+				mainMenuSelected = false;
+
+				purchase.purchaseMenuCall();
+			} else if(choice.equals(MAIN_MENU_OPTION_EXIT)){
+				break; //to close app
 			}
 		}
 	}
 
+
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		new CompileItemInventory(); //constructs product hash map at run through CompileInventory.java
 		cli.run();
 	}
+
 }
--- capstone/src/main/java/com/techelevator/view/Menu.java
+++ capstone/src/main/java/com/techelevator/view/Menu.java
@@ -3,13 +3,20 @@ package com.techelevator.view;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.*;
+
+import static com.techelevator.view.Purchase.feedMoney;
 
 public class Menu {
-
-	private PrintWriter out;
+	Purchase purchase = new Purchase();
+	static CompileItemInventory compile = new CompileItemInventory();
+	private static PrintWriter out;
 	private Scanner in;
+	public static Scanner userOderInput;
+	public static List<String> itemOdered;
+	public static String userOder;
 
+//ask how this constructor works
 	public Menu(InputStream input, OutputStream output) {
 		this.out = new PrintWriter(output);
 		this.in = new Scanner(input);
@@ -18,7 +25,11 @@ public class Menu {
 	public Object getChoiceFromOptions(Object[] options) {
 		Object choice = null;
 		while (choice == null) {
-			displayMenuOptions(options);
+			if(Arrays.equals(options, Purchase.PURCHASE_MENU_OPTIONS)){
+				displayPurchaseMenuOptions(options);
+			}else {
+				displayMenuOptions(options);
+			}
 			choice = getChoiceFromUserInput(options);
 		}
 		return choice;
@@ -28,7 +39,7 @@ public class Menu {
 		Object choice = null;
 		String userInput = in.nextLine();
 		try {
-			int selectedOption = Integer.valueOf(userInput);
+			int selectedOption = Integer.parseInt(userInput);
 			if (selectedOption > 0 && selectedOption <= options.length) {
 				choice = options[selectedOption - 1];
 			}
@@ -44,10 +55,69 @@ public class Menu {
 	private void displayMenuOptions(Object[] options) {
 		out.println();
 		for (int i = 0; i < options.length; i++) {
-			int optionNum = i + 1;
-			out.println(optionNum + ") " + options[i]);
+			int optionNumM = i + 1;
+			out.println(optionNumM + ") " + options[i]);
 		}
 		out.print(System.lineSeparator() + "Please choose an option >>> ");
 		out.flush();
+	}
+
+	private void displayPurchaseMenuOptions(Object[] options) {
+		out.println();
+		for (int i = 0; i < options.length; i++) {
+			int optionNumP = i + 1;
+			out.println(optionNumP + ") " + options[i]);
+		}
+		out.println(Purchase.PURCHASE_MENU_DISPLAY_MONEY_PROVIDED + Purchase.updateCurrentMoney());
+
+		out.flush();
+	}
+ // takes in money input and returns as string back to purchase.java
+	public static void moneyIntake(){
+		Scanner userMoneyInput = new Scanner(System.in);
+		String userMoney = userMoneyInput.nextLine();
+		feedMoney(userMoney);
+	}
+
+	public static void orderIntake(){
+		userOderInput = new Scanner(System.in);
+		userOder = userOderInput.nextLine();
+		System.out.println(compile.productMap.get(userOder));
+
+		List<String> itemOdered = compile.productMap.get(userOder);
+
+		if(Integer.parseInt(itemOdered.get(4)) != 0) {
+			int stock = Integer.parseInt(itemOdered.get(4)) - 1;
+			itemOdered.set(4, String.valueOf(stock));
+			compile.productMap.replace(userOder, itemOdered);
+
+		}
+
+		System.out.println(compile.productMap.get(userOder));
+
+		System.out.println("\nYou bought " + itemOdered.get(1) + " for " + itemOdered.get(2));
+		System.out.println(itemTypeMessage(itemOdered.get(3)));
+
+	}
+
+	public static void updateMap(String key, List<String> updatedList){
+		compile.productMap.replace(key, updatedList);
+	}
+
+	private static String itemTypeMessage(String type){
+		String message = "";
+
+		switch (type){
+			case "Gum" : message = "Chew Chew, Yum!";
+				break;
+			case "Chip" : message = "Crunch Crunch, Yum!";
+				break;
+			case "Candy" : message = "Munch Munch, Yum!";
+				break;
+			case "Drink" : message = "Glug Glug, Yum!";
+				break;
+		}
+
+		return message;
 	}
 }
