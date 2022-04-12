e0222173fd0ce888463fd7eb75e6b88811dbf423
Rhiannon <rmalloy789@gmail.com>
2022-04-11 22:36:55 -0700

    on comment 5

---
+++ capstone/src/main/java/com/techelevator/MachineItems.java
@@ -0,0 +1,37 @@
+package com.techelevator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+//1 - creating a class to extends to all item types
+public class MachineItems {
+    //2- creating the File object
+    private static final File itemList = new File("vendingmachine.csv");
+    //3- these maps will contain the items code, price, name, stock, and type
+    public static Map<String, String> itemCode = new HashMap<>();
+    public static Map<String, BigDecimal> itemPrice = new HashMap<>();
+    public static Map<String, String> itemName = new HashMap<>();
+    public static Map<String, Integer> itemStock = new HashMap<>();
+    public static Map<String, String> itemType =  new HashMap<>();
+    public static String[] itemsArray = new String[4];
+
+    //4- this method will read the file and store them in the appropriate maps
+
+    public static void readFile() throws FileNotFoundException {
+        try(Scanner fileReader = new Scanner(itemList)) {
+            while(fileReader.hasNextLine()) {
+                String fileLine = fileReader.nextLine();
+                itemsArray = fileLine.split("\\|");
+                itemCode.put(itemsArray[0], fileLine);
+                itemPrice.put(itemsArray[0], new BigDecimal(itemsArray[2]));
+                itemName.put(itemsArray[0], itemsArray[1]);
+                itemStock.put(itemsArray[0], 5);
+                itemType.put(itemsArray[0], itemsArray[3]);
+            }
+        }
+    }
+}
--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,11 +2,15 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
+import java.io.FileNotFoundException;
+
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String[] EXIT_BUTTON = {"Exit"};
+
 
 	private Menu menu;
 
@@ -14,7 +18,11 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
-	public void run() {
+
+
+	public void run() throws FileNotFoundException {
+		//5- running the vending machine file
+		MachineItems.readFile();
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
