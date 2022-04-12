6212516bd618f1bb8704aa499de97cbba850b0ab
Rhiannon <rmalloy789@gmail.com>
2022-04-12 11:16:39 -0700

    on comment 9

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -4,6 +4,8 @@ import com.techelevator.view.Menu;
 
 import java.io.FileNotFoundException;
 
+import static com.techelevator.MachineItems.*;
+
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
@@ -28,9 +30,15 @@ public class VendingMachineCLI {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
+				//7 - display vending machine items from file and with map
+				for(String f : itemCode.keySet()) {
+					System.out.println(f + "| " + itemName.get(f) + "| $" + itemPrice.get(f) + "| " + itemStock.get(f) + " in stock");
+				}
+				//9- this is an example of how we can update the stock
+				//itemStock.put("D4", itemStock.get("D4") - 1);
+				//System.out.println(itemStock.get("D4"));
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
+				//8 - do the purchase
 			} else if (choice.equals(EXIT_BUTTON)) {
 				//6 - added exit code
 				System.exit(3);
